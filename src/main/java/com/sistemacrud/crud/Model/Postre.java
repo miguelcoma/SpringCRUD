9ac0339e66fe072a0f55da1556efda37a24a3f8f
Miguel Colin <mcolinmi@gmail.com>
2022-10-03 20:02:03 -0500

    Conexion a la DB

---
+++ src/main/java/com/sistemacrud/crud/Model/Postre.java
@@ -0,0 +1,61 @@
+package com.sistemacrud.crud.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="postres")
+public class Postre {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	public String nombre;
+    public String precio;
+    public String stock;
+    public String img;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	@Override
+	public String toString() {
+		return "Postre [nombre=" + nombre + ", precio=" + precio + ", stock=" + stock + ", img=" + img + "]";
+	}
+    
+    
+    
+    
+}
